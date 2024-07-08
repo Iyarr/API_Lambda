59b6549a89a2a936479d65fb159cb691c155fe23
Iyarr <8.24ridiculous@gmail.com>
2024-07-08 12:23:40 +0900

    コンフリクトの解消

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
-import {
-  APIGatewayProxyEvent,
-  Context,
-  APIGatewayProxyResult,
-} from "aws-lambda";
-
-import { format } from "date-fns";
+import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import { validateHeader } from "./auth.js";
 
 export const handler = async (
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
   return new Promise((resolve, reject) => {
-    resolve({
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message: "OK",
-        event: event,
-        date: format(new Date(), "yyyy-MM-dd"),
-      }),
-    });
-    return;
+    try {
+      validateHeader(event.headers);
+      resolve({
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: "OK",
+        }),
+      });
+    } catch (error) {
+      resolve({
+        statusCode: error.statusCode,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: error.message,
+        }),
+      });
+    }
   });
 };
